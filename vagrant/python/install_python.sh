e2800dfac8391dd001b1a87695d26246e4de1720
AdrienCeschin <adrien.ceschin.devops@gmail.com>
2021-07-08 16:53:58 +0200

    US-30-environnement-developpement-initial : mise à jour du install-python.sh avec modules pythons.

--- vagrant/python/install_python.sh
+++ vagrant/python/install_python.sh
@@ -2,4 +2,9 @@
 apt-get update
 # install python
 apt-get install python3 python3-dev python3-pip -q -y
+# install git for later GitPython module
+apt-get install -y -q git
+# remove python to keep only python 3
 apt remove -y python
+# install all needed python modules for the project (markdown to convert .m files to .html, GitPython to communicate with GitHub, pylint for code quality testing, flask for web server dev)
+pip3 install flask markdown pylint GitPython
